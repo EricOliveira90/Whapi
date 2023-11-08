75902a692aeac7a68f0a811e6b328a18d1aaa6a5
Eric Oliveira <ericfaria@gmail.com>
2023-11-07 23:19:12 -0300

    feat: receive and save file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Import the Flask and requests modules
 from flask import Flask, request, make_response
 import requests
+import os
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -14,17 +15,43 @@ def hello_world():
 # Define a route for the POST request
 @app.route("/upload", methods=["POST"])
 def upload_file():
+    # Access request data
+    request_data = request.data
+    request_headers = request.headers
+    request_args = request.args
+    request_form = request.form
+    request_files = request.files
+
+    # Print request data for debugging
+    print("Received request data:")
+    print(f"Request method: {request.method}")
+    print(f"Request URL: {request.url}")
+    print(f"Request headers: {request_headers}")
+    print(f"Request args: {request_args}")
+    print(f"Request form data: {request_form}")
+    print(f"Request body: {request_data}")
+    print(f"Request files: {request_files}")
+
+    uploaded_file = request_files['file']
+    if uploaded_file:  # Check if a file was provided
+        folder_path = 'audios'
+        file_path = os.path.join(folder_path, uploaded_file.filename)  # Define the target file path
+        uploaded_file.save(file_path)  # Save the uploaded file to the specified folder
+        return 'File uploaded and saved successfully.'
+    return 'No file provided in the request.'
+
+
     # Get the mp3 file from the request
-    file = request.files.get("audio")
+    # file = request.files.get("audio")
     # Check if the file is valid
-    if file and file.filename.endswith(".mp3"):
-        # Save the file to a local directory
-        file.save("uploads/" + file.filename)
-        # Return a "ok" message with HTTP status 200
-        return make_response("ok", 200)
-    else:
-        # Return a "bad request" message with HTTP status 400
-        return make_response("bad request", 400)
+    # if file and file.filename.endswith(".mp3"):
+    #     # Save the file to a local directory
+    #     file.save("uploads/" + file.filename)
+    #     # Return a "ok" message with HTTP status 200
+    #      return make_response("ok", 200)
+    # else:
+    #     # Return a "bad request" message with HTTP status 400
+    #     return make_response("bad request", 400)
 
 # Run the app in debug mode
 if __name__ == "__main__":
--- test_route.py
+++ test_route.py
@@ -2,19 +2,37 @@
 import requests
 
 # Define the URL to send the request to
-url = "http://127.0.0.1:5000/"
+url = "http://127.0.0.1:5000/upload"
 
-# Send a GET request to the URL and store the response object
-response = requests.get(url)
+def hello_world():
+    # Send a GET request to the URL and store the response object
+    response = requests.get(url)
 
-# Check the status code of the response
-if response.status_code == 200:
-    # The request was successful
-    print("The request was successful.")
-    # Print the content of the response
-    print(response.content)
-else:
-    # The request failed
-    print("The request failed.")
-    # Print the status code and the reason
-    print(response.status_code, response.reason)
+    # Check the status code of the response
+    if response.status_code == 200:
+        # The request was successful
+        print("The request was successful.")
+        # Print the content of the response
+        print(response.content)
+    else:
+        # The request failed
+        print("The request failed.")
+        # Print the status code and the reason
+        print(response.status_code, response.reason)
+
+def send_audio(file_path):
+    # Create a dictionary to specify the file to be uploaded
+    files = {'file': ('audio.mp3', open(file_path, 'rb'), 'audio/mpeg')}
+
+    # Send the POST request with the MP3 file and additional data (if any)
+    response = requests.post(url, files=files)
+
+    # Check the response status and content
+    if response.status_code == 200:
+        print("POST request successful")
+        print("Response content:")
+        print(response.text)
+    else:
+        print("POST request failed with status code:", response.status_code)
+
+send_audio("sample-3s.mp3")
